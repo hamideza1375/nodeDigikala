4dcc9b5a7fdbcc19038ea5ef4c8c2b2c3262d78e
hamid reza <reza.attar1375@gmail.com>
2023-06-06 09:41:52 +0430

    authAdmin

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -6,9 +6,9 @@ const { UserModel } = require('../model/UserModel');
 
 module.exports = async (req, res, next) => {
   const user = jwt.decode(req.header('Authorization'), { complete: true });
-  if (!user) return res.status(400).send('شما هنوز ثبت نام نکرده اید')
+  if (!user) return res.status(401).send('شما هنوز ثبت نام نکرده اید')
   const usermodel = await UserModel.findById(user.payload.userId)
-  if (!usermodel) return res.status(400).send('شما هنوز ثبت نام نکرده اید')
+  if (!usermodel) return res.status(401).send('شما هنوز ثبت نام نکرده اید')
   req.user = user
   next()
 };
